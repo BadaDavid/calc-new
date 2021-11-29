7bb3a2250b2c06e65662624d3106536416b97607
Bada O. David <badadavid04@gmail.com>
2021-11-29 13:21:03 +0100

    master

--- login.php
+++ login.php
@@ -1,25 +1,9 @@
 <?php
-    session_start();
-    $link = mysqli_connect('localhost','root','08061646761','backend2');
-    $fetch_user =  $fetch_pass = "";
-    if (isset($_POST['signin'])) {
-        $user = $_POST['user'];
-        $pass = $_POST['pass'];
-        $sql_check = "SELECT * FROM registration_form WHERE username = '$user' AND password = '$pass' ";
-        $check = mysqli_query($link,$sql_check);
-       while ($row = mysqli_fetch_assoc($check)) {
-           $fetch_user = $row['username'];
-           $fetch_pass = $row['password'];
-        
-        }
-            if ( $fetch_user == "" ||  $fetch_pass =="" ) {
-            echo "<script>alert('Incorrect Login Citerail')</script>";
-            }
-            else {
-                header("location: indexphp");
-            }
     
-    }
+   if (isset($_POST['press'])){
+       echo "<script>alert('good boy')</script>";
+   }
+   
 ?>
 
 <!DOCTYPE html>
@@ -36,19 +20,19 @@
 </head>
 <body>
     
-    <form action=""  style="background-color:lightgreen;" class="mx-auto shadow form-controls col-6 mt-5 py-4 px-3 border" name="login">
+    <form action="login.php"  style="background-color:lightgreen;" class="mx-auto shadow col-6 mt-5 py-4 px-3" name="login">
         <h3 class="text-center">LOGIN HERE</h3>
         <label for="username" class="form-label">USERNAME:</label>
-        <input type="text" name="user" class="form-control" required maxlength="20">
+        <input type="text" name="user" class="form-control" maxlength="20">
         <br>
         <label for="password" class="form-label">PASSWORD:</label>
-        <input type="password" name="pass" class="form-control" required maxlength="20">
+        <input type="password" name="pass" class="form-control" maxlength="20">
         <br>
-        <input type="submit" value="LOGIN" name="signin" class="btn btn-success">
+        <input type="submit" value="LOGIN" name="press" class="btn btn-success">
     </form>
 
     <script src="plugins/bootstrap/assets/js/vendor/jquery-slim.min.js"></script>
-    <script src="/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
     <script>
         new WOW().init()
     </script>
